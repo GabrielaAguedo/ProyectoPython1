750b6c52bd584cee6fd7460c8272fde886f466e5
GabrielaAguedo <gorby9220@gmail.com>
2022-05-10 01:00:55 -0300

    agregando carrito

--- proyecto2.py
+++ proyecto2.py
@@ -1,25 +1,75 @@
-global lista
-lista = list()
+from curses.ascii import SI
+from msilib.schema import Class
 
+
+global listaP
+global listaCarrito
+listaP = list()
+listaCarrito = list()
 class Producto :
     id = ""
     nombre = ""
     precio = 0
+    cantidad_p = 0
+
+class Carrito :
+    id_compra = ""
+    product_compra = ""
+    precio_prod = 0
+    cantidad_prod = 0
+    total_pago = 0
 
 def registrar_producto () :
     print("Registro de Productos")
+    prod = Producto()
 
+    prod.id = input("Ingrese Identificador del Producto: ")
+    prod.nombre = input("Ingrese Nombre del Producto: ")
+    prod.precio = int(input("Ingrese el Precio del Producto: "))
+    prod.cantidad_p = int(input("Ingrese la Cantidad de Productos a Agregar: "))
+    listaP.append(prod)
 
 def listar_producto () :
     print("Listado de Productos")
+    for prod in listaP :
+        print(prod.id, "--", prod.nombre, "--", prod.precio, "--", prod.cantidad_p)
 
 
 def buscar_producto () :
     print("Busqueda de Productos")
+    id = input("Ingrese el id del Producto a buscar: ")
+
+    for prod in listaP :
+        if prod.id == id :
+            print(prod.id, "--", prod.nombre, "--", prod.precio, "--", prod.cantidad_p)
 
 
 def agregar_carrito () :
     print("Agregar Producto al Carrito")
+    carrito_com = Carrito()
+    id = input("Ingrese Identificador del Producto: ")
+    for prod in listaP :
+        if prod.id == id :
+            carrito_com.product_compra = prod.nombre
+            carrito_com.precio_prod = prod.precio
+            carrito_com.cantidad_prod = int(input("Ingrese la Cantidad de Productos a Agregar: "))
+            carrito_com.total_pago = (carrito_com.precio_prod * carrito_com.cantidad_prod)
+            resp = input("Desea Agregar otro producto al carrito")
+            if (resp == SI) :
+                id = input("Ingrese Identificador del Producto: ")
+                for prod in listaP :
+                    if prod.id == id :
+                        carrito_com.product_compra = prod.nombre
+                        carrito_com.precio_prod = prod.precio
+                        carrito_com.cantidad_prod = int(input("Ingrese la Cantidad de Productos a Agregar: "))
+                        carrito_com.total_pago = (carrito_com.precio_prod * carrito_com.cantidad_prod)
+            else :
+                print("LA TIENDA")
+                print(carrito_com.product_compra, "----------", carrito_com.precio_prod)
+                print("TOTAL: ", carrito_com.total_pago)
+
+    
+
 
 
 def remover_carrito() :
@@ -29,7 +79,6 @@ def remover_carrito() :
 def salir () :
     print("Gracias por usar la aplicación")
     
-
 
 def menu ():
 
